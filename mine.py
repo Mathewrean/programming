f03ed76438a2ec733056c6a67e063e9191b1995c
Mathewrean <mathewrean@gmail.com>
2026-06-23 12:03:54 +0300

    learned dictionary in py

---
+++ mine.py
@@ -0,0 +1,29 @@
+# # Dictionaries and listed dictionary
+
+# names = {
+#     "name1": {
+#         "nam": "john",
+#         "age": 30,
+#         "weight": 70
+#     },
+
+#     "name2": {
+#         "nam": "jane",
+#         "age": 25,
+#         "weight": 60
+#     },
+# }
+
+# # print single item
+# print(names["name1"])
+
+# # printing all dictionary items in table manner. (<dictionary name>[<iteratable value>[<dictionary key> | etc.]])
+# for name in names:
+#     print(f"{names[name]['nam']:<12} | {names[name]['age']:<3} | {names[name]['weight']:<3}")
+
+
+
+# List
+
+listy = ["apple", "banana", "cherry", "date"]
+print
