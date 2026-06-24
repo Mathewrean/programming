5590e925d54bddbb7e04048fc936f312969c75a2
Mathewrean <mathewrean@gmail.com>
2026-06-24 14:47:01 +0300

    variables and values

--- MyfirstC#/Program.cs
+++ MyfirstC#/Program.cs
@@ -33,21 +33,36 @@
 //     } 
 // }
 
-using  System;
+// using  System;
+
+// class Program
+// {
+//     static void Main()
+// {
+//     bool statussing = true;
+//     double salary = 468982.443242;
+//     int months = 24;
+
+//     double wage = salary * months;
+//         Console.WriteLine("Ths is he first Line");
+//         Console.WriteLine("This is the second line");
+//         Console.WriteLine(statussing);
+//         Console.WriteLine(salary);
+//         Console.WriteLine(wage);
+
+
+//     }
+// }
+
+
+
+using System;
 
 class Program
 {
     static void Main()
-{
-    bool statussing = true;
-    double salary = 468982.443242;
-    int months = 24;
-
-    double wage = salary * months;
-        Console.WriteLine("Ths is he first Line");
-        Console.WriteLine("This is the second line");
-        Console.WriteLine(statussing);
-        Console.WriteLine(salary);
-        Console.WriteLine(wage);
+    {
+        Console.WriteLine("We resume maths if any exis....");
+        
     }
 }
